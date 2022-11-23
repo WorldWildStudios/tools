4ba1a0904ec5d2ddaada4043b690edab8bbabe21
ClassyCrafter <classy.craft3r@gmail.com>
2022-11-23 15:03:21 +0100

    Update Main.java

--- fr/sayonedevs/tools/main/Main.java
+++ fr/sayonedevs/tools/main/Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world! This is a plugin ddev by WebOsys Team");
+    System.out.println("Hello world! Plugin in development!");
   }
 }
